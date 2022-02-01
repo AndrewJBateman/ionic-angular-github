b5dc559dbbd5403c913fc00222976b80b3a4337a
AndrewJBateman <gomezbateman@yahoo.com>
2022-02-01 11:07:13 +0100

    chore: reformatting with prettier

--- src/app/pages/favourites/favourites.page.ts
+++ src/app/pages/favourites/favourites.page.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-favourites',
-  templateUrl: './favourites.page.html',
-  styleUrls: ['./favourites.page.scss'],
+  selector: "app-favourites",
+  templateUrl: "./favourites.page.html",
+  styleUrls: ["./favourites.page.scss"],
 })
 export class FavouritesPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-	selector: 'app-tabs',
-	templateUrl: './tabs.page.html',
-	styleUrls: ['./tabs.page.scss'],
+  selector: "app-tabs",
+  templateUrl: "./tabs.page.html",
+  styleUrls: ["./tabs.page.scss"],
 })
 export class TabsPage implements OnInit {
+  constructor() {}
 
-	constructor() { }
-
-	ngOnInit() {
-	}
-
+  ngOnInit() {}
 }
